34d01c6b77dd0e31d9ad4d8c4199df1bdf1f2c88
Vikram Chatterjee <strycora@gmail.com>
2021-01-28 13:08:18 -0700

    commit tree 83ac5cace6963274e5e73a69fd612f30dd492b79

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'welcome#home'
 
+  match '/auth/:google_oauth2/callback' => 'sessions#google', via:[:get, :post]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
